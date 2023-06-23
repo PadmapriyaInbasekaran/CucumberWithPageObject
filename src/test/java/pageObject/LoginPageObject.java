c2b9e500028daf49351374c22da1f259fe7b6097
Padmapriya Inbasekaran <Padmapriya.Inbasekaran@ATMECS.CORP>
2023-06-23 21:53:43 +0530

    first commit

--- src/test/java/pageObject/LoginPageObject.java
+++ src/test/java/pageObject/LoginPageObject.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import utilities.UtilitiesClass;
-
 import java.util.Properties;
-
 import static stepDefinition.BaseTestClass.driver;
 
 public class LoginPageObject {
